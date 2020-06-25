7380f45ecb57e078e3f1424e5fb57f8997b4449e
Nahuel Espiño <estudio@nahuelespino.com.ar>
2020-06-25 03:07:16 -0300

    Template added

---
+++ app/pokemon.tpl.php
@@ -0,0 +1,11 @@
+<div class="row mt-5 ml-2 col-12">
+    <div class="col-3">
+        <img src="img/no-image.png">
+    </div>
+    <div class="col-3 pt-3">
+        <span class="pokemon-name">
+            {{pokemonName}}
+        </span>
+    </div>
+    <div class="col-6"></div>
+</div>
